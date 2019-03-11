a494bcd03cddd945e3b9f1990acff4a36999590a
NoemiSz <szokenoemijob@gmail.com>
2019-03-11 20:08:23 +0100

    DicerollApplication created

---
+++ diceroll/src/main/java/com/codecool/diceroll/controller/DiceRollController.java
@@ -0,0 +1,53 @@
+package com.codecool.diceroll.controller;
+
+import com.codecool.diceroll.service.DiceRollService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.core.SpringProperties.getProperty;
+
+@RestController
+@Slf4j
+@RequestMapping("/roll")
+public class DiceRollController {
+
+    @Autowired
+    private DiceRollService service;
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/d6")
+    public Roll rollD6(){
+        return new Roll(
+                service.rollD6(),
+                "D6",
+                env.getProperty("server.port")
+        );
+    }
+    @GetMapping("/d20")
+    public Roll rollD20(){
+        return new Roll(
+                service.rollD20(),
+                "D20",
+                env.getProperty("server.port")
+        );
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class Roll{
+        private int result;
+        private String dice;
+        private String port;
+    }
+}
