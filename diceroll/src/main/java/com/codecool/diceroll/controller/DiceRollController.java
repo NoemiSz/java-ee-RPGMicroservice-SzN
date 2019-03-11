758a3c4db73baefc1dbaf79ece883dd172d86e4e
NoemiSz <szokenoemijob@gmail.com>
2019-03-11 22:38:40 +0100

    ServiceRegistryTest skeleton added

--- diceroll/src/main/java/com/codecool/diceroll/controller/DiceRollController.java
+++ diceroll/src/main/java/com/codecool/diceroll/controller/DiceRollController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.core.SpringProperties.getProperty;
-
 @RestController
 @Slf4j
 @RequestMapping("/roll")
---
+++ diceroll1/src/main/java/com/codecool/diceroll1/Diceroll1Application.java
@@ -0,0 +1,13 @@
+package com.codecool.diceroll1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Diceroll1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Diceroll1Application.class, args);
+    }
+
+}
